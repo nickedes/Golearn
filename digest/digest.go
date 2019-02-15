c5e68da5914ead7d9b2a745ec1ad64af56b6a500
nickedes <nkmittal4994@gmail.com>
2019-02-15 21:45:18 +0530

    checks if hash starts with 0

--- digest/digest.go
+++ digest/digest.go
@@ -3,22 +3,30 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func Digest() string {
 	nonce := 0
 
+	d := 4
+	x := ""
+	for i := 0; i < d; i++ {
+		x += "0"
+	}
+
 	digest := sha256.New()
-	s := "text string here!"
+	s := "t1ext string here!"
 	digest.Write([]byte(s))
 
 	for {
-		digest.Write([]byte(nonce))
+		digest.Write([]byte(strconv.Itoa(nonce)))
 		hashval := fmt.Sprintf("%x\n", digest.Sum(nil))
-		if hashval {
+		if strings.HasPrefix(hashval, x) {
 			return hashval
 		}
-
+		nonce += 1
 	}
 }
 

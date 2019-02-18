0453938f45a2f5087acfcd0665cfed3c148c931b
nickedes <nkmittal4994@gmail.com>
2019-02-18 19:59:10 +0530

    sum using goroutines and channels

--- GoRoutines/routine.go
+++ GoRoutines/routine.go
@@ -12,7 +12,25 @@ func say(s string) {
 	}
 }
 
+func sum(a []int, c chan int) {
+	sum := 0
+	for _, v := range a {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
 func main() {
 	go say("world")
-	//say("hello")
+	say("hello")
+
+	a := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(a[:len(a)/2], c)
+	go sum(a[len(a)/2:], c)
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
+
 }

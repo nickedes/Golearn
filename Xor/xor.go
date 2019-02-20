2c409c5d59650ce172857c3aef750a88bedc797c
nickedes <nkmittal4994@gmail.com>
2019-02-21 02:25:56 +0530

    sample strings from list of strings

--- Xor/xor.go
+++ Xor/xor.go
@@ -3,19 +3,33 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
-func sample(A []int, x int) []int {
+func sample(A []string, x int) []string {
 	rand.Seed(time.Now().UnixNano())
 	randomize := rand.Perm(len(A))
 
-	sampleslice := make([]int, 0)
+	sampleslice := make([]string, 0)
 
 	for _, v := range randomize[:x] {
 		sampleslice = append(sampleslice, A[v])
 	}
 	return sampleslice
+}
+
+func xorbinary(A []string) int64 {
+	var xorVal int64
+	for i := range A {
+		intval, _ := strconv.ParseInt(A[i], 2, 0)
+		xorVal = xorVal ^ intval
+	}
+	return xorVal
+}
+
+func getXor(A []string) (string, []string) {
+
 }
 
 func main() {

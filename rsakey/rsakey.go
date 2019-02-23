c2d5e253fa99d53dba05deccc08c0c805abb8aac
nickedes <nkmittal4994@gmail.com>
2019-02-23 16:51:48 +0530

    get rsa public key and report error

--- rsakey/rsakey.go
+++ rsakey/rsakey.go
@@ -3,14 +3,16 @@ package main
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"encoding/json"
 	"fmt"
+	"math/big"
 )
 
 func main() {
 	fmt.Println("Start!")
-	rsakey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err == nil {
-		publicKey := rsakey.Public() // get the public key from the rsa key
+	rsakey, errKey := rsa.GenerateKey(rand.Reader, 2048)
+	if errKey == nil {
+		publicKey := rsakey.Public()               // get the public key from the rsa key
 		rsaPublicKey := publicKey.(*rsa.PublicKey) // convert to rsa publick key type
 		fmt.Println("N : ", rsaPublicKey.N)
 		fmt.Println("E : ", rsaPublicKey.E)

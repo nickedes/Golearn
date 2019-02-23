9959acc5766b2caa919f886531f163b05e340146
nickedes <nkmittal4994@gmail.com>
2019-02-23 16:52:19 +0530

    decode and encode key

--- rsakey/rsakey.go
+++ rsakey/rsakey.go
@@ -14,8 +14,21 @@ func main() {
 	if errKey == nil {
 		publicKey := rsakey.Public()               // get the public key from the rsa key
 		rsaPublicKey := publicKey.(*rsa.PublicKey) // convert to rsa publick key type
-		fmt.Println("N : ", rsaPublicKey.N)
-		fmt.Println("E : ", rsaPublicKey.E)
+		// fmt.Println("N : ", rsaPublicKey.N)
+		// fmt.Println("E : ", rsaPublicKey.E)
+		// send N
+		encoded, errMarshal := json.Marshal(rsaPublicKey.N) // encode the N param
+		if errMarshal == nil {
+			decoded := big.NewInt(0)
+			errUnmarshall := json.Unmarshal(encoded, &decoded)
+			if errUnmarshall == nil {
+				fmt.Println(decoded.Cmp(rsaPublicKey.N) == 0)
+			} else {
+				fmt.Println("UnMarshal error", errUnmarshall)
+			}
+		} else {
+			fmt.Println("Marshal error", errMarshal)
+		}
 	} else {
 		fmt.Println("Rsa key generation Error!", err)
 	}

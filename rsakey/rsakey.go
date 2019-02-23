33cae1d52a7ae5f3b55659462c4fc795492ed366
nickedes <nkmittal4994@gmail.com>
2019-02-23 16:26:01 +0530

    get the public key of rsa type

--- rsakey/rsakey.go
+++ rsakey/rsakey.go
@@ -9,9 +9,12 @@ import (
 func main() {
 	fmt.Println("Start!")
 	rsakey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		fmt.Println(rsakey)
+	if err == nil {
+		publicKey := rsakey.Public() // get the public key from the rsa key
+		rsaPublicKey := publicKey.(*rsa.PublicKey) // convert to rsa publick key type
+		fmt.Println("N : ", rsaPublicKey.N)
+		fmt.Println("E : ", rsaPublicKey.E)
 	} else {
-		fmt.Println("Rsa key generation Error!")
+		fmt.Println("Rsa key generation Error!", err)
 	}
 }
